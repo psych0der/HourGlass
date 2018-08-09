ecb88d614b5033d2fedfd4c20f31d47b89984288
psych0der <mayankbhola@gmail.com>
2018-08-09 18:32:10 +0530

    Create JWT strategy for passport

---
+++ src/server/config/passport.js
@@ -0,0 +1,21 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
+const { jwtSecret } = require('./vars');
+const User = require('../api/models/User.js');
+
+const jwtOptions = {
+  secretOrKey: jwtSecret,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+};
+
+const jwt = async (payload, done) => {
+  try {
+    const user = await User.findById(payload.sub);
+    if (user) return done(null, user);
+    return done(null, false);
+  } catch (error) {
+    return done(error, false);
+  }
+};
+
+exports.jwt = new JwtStrategy(jwtOptions, jwt);
