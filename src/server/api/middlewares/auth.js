f887050e30cedbe20904961d4e06eba29caec86d
psych0der <mayankbhola@gmail.com>
2018-08-15 01:26:38 +0530

    export user-manager

--- src/server/api/middlewares/auth.js
+++ src/server/api/middlewares/auth.js
@@ -5,7 +5,7 @@ const APIError = require('../utils/APIError');
 
 const SUPER_ADMIN = 'super-admin';
 const USER_MANAGER = 'user-manager';
-const LOGGED_USER = '_loggedUser';
+const LOGGED_USER = '_logged-user';
 
 const handleJWT = (req, res, next, roles) => async (err, user, info) => {
   const error = err || info;
@@ -16,6 +16,7 @@ const handleJWT = (req, res, next, roles) => async (err, user, info) => {
     stack: error ? error.stack : undefined,
   });
 
+  // return error if user is not available or error is present
   try {
     if (error || !user) throw error;
     await logIn(user, { session: false });
@@ -23,17 +24,23 @@ const handleJWT = (req, res, next, roles) => async (err, user, info) => {
     return next(apiError);
   }
 
-  if (roles === LOGGED_USER) {
-    if (user.role !== 'admin' && req.params.userId !== user._id.toString()) {
-      apiError.status = httpStatus.FORBIDDEN;
-      apiError.message = 'Forbidden';
-      return next(apiError);
-    }
-  } else if (!roles.includes(user.role)) {
+  // return error if token's user doesn't match with userId in param
+  // and user is not Super admin or user manager
+
+  if (
+    ![SUPER_ADMIN, USER_MANAGER].includes(user.role) &&
+    req.params.userId !== user._id.toString()
+  ) {
     apiError.status = httpStatus.FORBIDDEN;
     apiError.message = 'Forbidden';
     return next(apiError);
-  } else if (err || !user) {
+  }
+  if (!roles.includes(user.role)) {
+    apiError.status = httpStatus.FORBIDDEN;
+    apiError.message = 'Forbidden';
+    return next(apiError);
+  }
+  if (err || !user) {
     return next(apiError);
   }
 
@@ -42,9 +49,11 @@ const handleJWT = (req, res, next, roles) => async (err, user, info) => {
   return next();
 };
 
-exports.ADMIN = SUPER_ADMIN;
+exports.SUPER_ADMIN = SUPER_ADMIN;
+exports.USER_MANAGER = USER_MANAGER;
 exports.LOGGED_USER = LOGGED_USER;
 
+/* Pass all roles if not roles specified for a particular request */
 exports.authorize = (roles = User.roles) => (req, res, next) =>
   passport.authenticate(
     'jwt',
