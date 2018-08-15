03a9278cec29d72aa592a0305bbf1e035cdebbd7
psych0der <mayankbhola@gmail.com>
2018-08-15 19:11:06 +0530

    Add flag to specify only user or Admin access to APIs

--- src/server/api/middlewares/auth.js
+++ src/server/api/middlewares/auth.js
@@ -7,7 +7,11 @@ const SUPER_ADMIN = 'super-admin';
 const USER_MANAGER = 'user-manager';
 const LOGGED_USER = '_logged-user';
 
-const handleJWT = (req, res, next, roles) => async (err, user, info) => {
+const handleJWT = (req, res, next, roles, onlyUserOrAdmin) => async (
+  err,
+  user,
+  info
+) => {
   const error = err || info;
   const logIn = Promise.promisify(req.logIn);
   const apiError = new APIError({
@@ -26,9 +30,11 @@ const handleJWT = (req, res, next, roles) => async (err, user, info) => {
 
   // return error if token's user doesn't match with userId in param
   // and user is not Super admin or user manager
-
+  const allowedActors = onlyUserOrAdmin
+    ? [SUPER_ADMIN]
+    : [SUPER_ADMIN, USER_MANAGER];
   if (
-    ![SUPER_ADMIN, USER_MANAGER].includes(user.role) &&
+    !allowedActors.includes(user.role) &&
     req.params.userId !== user._id.toString()
   ) {
     apiError.status = httpStatus.FORBIDDEN;
@@ -53,10 +59,20 @@ exports.SUPER_ADMIN = SUPER_ADMIN;
 exports.USER_MANAGER = USER_MANAGER;
 exports.LOGGED_USER = LOGGED_USER;
 
-/* Pass all roles if not roles specified for a particular request */
-exports.authorize = (roles = User.roles) => (req, res, next) =>
+/**
+ * Authorization middleware for requests
+ *  @param{Array} roles list of possible roles for user
+ *  @param{Boolean} onlyUserOrAdmin specifies if only user itself or super-admin
+ *  is allowed to request/manipulate resources of the user. This refers to requests URIs containing
+ *  userId
+ */
+exports.authorize = (roles = User.roles, onlyUserOrAdmin = false) => (
+  req,
+  res,
+  next
+) =>
   passport.authenticate(
     'jwt',
     { session: false },
-    handleJWT(req, res, next, roles)
+    handleJWT(req, res, next, roles, onlyUserOrAdmin)
   )(req, res, next);
