f8c9b44e86595e497ad480f2bf384c2daad97bd5
psych0der <mayankbhola@gmail.com>
2018-08-17 03:16:44 +0530

    load user from localstorage

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { ConnectedRouter } from 'connected-react-router';
-import store, { history } from './redux/store';
+import { store, persistor, history } from './redux/store';
+import { setCurrentUser } from './redux/reducers/auth';
 import { App } from './containers';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -12,12 +14,24 @@ import './index.css';
 
 const target = document.querySelector('#root');
 
+// try to fetch user from localstorage
+if (localStorage.user) {
+  const userObject = JSON.parse(localStorage.user);
+  if (new Date(userObject.expiresIn) < new Date()) {
+    localStorage.removeItem('user');
+  } else {
+    store.dispatch(setCurrentUser(userObject));
+  }
+}
+
 render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
       <div>
         <App />
       </div>
+      {/* </PersistGate> */}
     </ConnectedRouter>
   </Provider>,
   target
