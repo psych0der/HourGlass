6c4d50dce8556a01864d8dae8182e663379837d5
psych0der <mayankbhola@gmail.com>
2018-08-17 00:23:14 +0530

    formating changes

--- src/client/redux/middlewares/augmentorMiddleware.js
+++ src/client/redux/middlewares/augmentorMiddleware.js
@@ -11,32 +11,30 @@
  */
 
 export default function augmentorMiddleware() {
-  return ({ dispatch, getState }) => {
-    return next => action => {
-      if (typeof action === 'function') {
-        return action(dispatch, getState);
-      }
+  return ({ dispatch, getState }) => next => action => {
+    if (typeof action === 'function') {
+      return action(dispatch, getState);
+    }
 
-      const { promise, types, ...rest } = action; // eslint-disable-line no-redeclare
-      if (!promise) {
-        return next(action);
-      }
+    const { promise, types, ...rest } = action; // eslint-disable-line no-redeclare
+    if (!promise) {
+      return next(action);
+    }
 
-      const [REQUEST, SUCCESS, FAILURE] = types;
-      next({ ...rest, type: REQUEST });
+    const [REQUEST, SUCCESS, FAILURE] = types;
+    next({ ...rest, type: REQUEST });
 
-      const actionPromise = promise();
-      actionPromise
-        .then(
-          result => next({ ...rest, result, type: SUCCESS }),
-          error => next({ ...rest, error, type: FAILURE })
-        )
-        .catch(error => {
-          console.error('MIDDLEWARE ERROR:', error);
-          next({ ...rest, error, type: FAILURE });
-        });
+    const actionPromise = promise();
+    actionPromise
+      .then(
+        result => next({ ...rest, result, type: SUCCESS }),
+        error => next({ ...rest, error, type: FAILURE })
+      )
+      .catch(error => {
+        console.error('MIDDLEWARE ERROR:', error);
+        next({ ...rest, error, type: FAILURE });
+      });
 
-      return actionPromise;
-    };
+    return actionPromise;
   };
 }
