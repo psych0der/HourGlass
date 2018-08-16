3c741bb8de4e1c36702e7e3711df69ebb6e2a27d
psych0der <mayankbhola@gmail.com>
2018-08-17 03:15:38 +0530

    add loading component

--- src/client/components/index.js
+++ src/client/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from './Navbar';
 export { default as NotFound } from './NotFound';
 export { default as LoaderButton } from './LoaderButton';
+export { default as LoadingComponent } from './LoadingComponent';
