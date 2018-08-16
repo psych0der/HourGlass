051226d2b872dec5a29f4ee347ed29aad07183be
psych0der <mayankbhola@gmail.com>
2018-08-17 03:15:06 +0530

    add redux-auth-wrapper to app

--- src/client/containers/App/index.js
+++ src/client/containers/App/index.js
@@ -1,13 +1,51 @@
 // @flow
 import React from 'react';
-import { Route } from 'react-router-dom';
 import Routes from '../../Routes';
+import { AUTH_BLANK_STATE } from '../../redux/reducers/auth';
+import { IDLE, IN_PROGRESS, SUCCESS, FAILED } from '../../commons/constants';
 import { Link } from 'react-router-dom';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import connectedAuthWrapper from 'redux-auth-wrapper/connectedAuthWrapper';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { store } from '../../redux/store';
 import './index.css';
-// import Home from '../Home';
 
+const userIsAuthenticated = connectedAuthWrapper({
+  authenticatedSelector: state => state.auth.user !== null,
+  authenticatingSelector: state => state.auth.status === IN_PROGRESS,
+  // A nice display name for this check
+  wrapperDisplayName: 'UserIsAuthenticated',
+});
+
+const userIsNotAuthenticated = connectedAuthWrapper({
+  authenticatedSelector: state =>
+    state.auth.user === null && state.auth.status !== IN_PROGRESS,
+  wrapperDisplayName: 'UserIsNotAuthenticated',
+});
+
+// Only show login when the user is not logged in and logout when logged in
+// Could have also done this with a single wrapper and `FailureComponent`
+
+const LoginLink = userIsNotAuthenticated(() => (
+  <LinkContainer to="/login">
+    <NavItem>Login</NavItem>
+  </LinkContainer>
+));
+const SignupLink = userIsNotAuthenticated(() => (
+  <LinkContainer to="/signup">
+    <NavItem>Signup</NavItem>
+  </LinkContainer>
+));
+
+const LogoutLink = userIsAuthenticated(() => {
+  return (
+    <NavItem onClick={() => store.dispatch({ type: AUTH_BLANK_STATE })}>
+      Logout
+    </NavItem>
+  );
+});
 type Props = {};
 type State = {};
 
@@ -24,12 +62,9 @@ export class App extends React.Component<Props, State> {
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav pullRight>
-              <LinkContainer to="/signup">
-                <NavItem>Signup</NavItem>
-              </LinkContainer>
-              <LinkContainer to="/login">
-                <NavItem>Login</NavItem>
-              </LinkContainer>
+              <LoginLink />
+              <LogoutLink />
+              <SignupLink />
             </Nav>
           </Navbar.Collapse>
         </Navbar>
