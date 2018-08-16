c82b6217dd107f2c31a9acc3ae8501d6f18d57f3
psych0der <mayankbhola@gmail.com>
2018-08-17 03:17:25 +0530

    add redux persist

--- src/client/redux/store.js
+++ src/client/redux/store.js
@@ -3,8 +3,16 @@ import { connectRouter, routerMiddleware } from 'connected-react-router';
 import augmentorMiddleware from './middlewares/augmentorMiddleware';
 import createHistory from 'history/createBrowserHistory';
 import rootReducer from './reducers';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
 
 export const history = createHistory();
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['navigation'],
+};
 
 const initialState = {};
 const enhancers = [];
@@ -22,9 +30,27 @@ const composedEnhancers = compose(
   applyMiddleware(...middleware),
   ...enhancers
 );
+const persistedReducer = persistReducer(
+  persistConfig,
+  connectRouter(history)(rootReducer)
+);
 
-export default createStore(
+export const store = createStore(
+  //persistedReducer,
   connectRouter(history)(rootReducer),
   initialState,
   composedEnhancers
 );
+
+// enabling hot module replacement
+// if (module.hot) {
+//   module.hot.accept(() => {
+//     // This fetch the new state of the above reducers.
+//     const nextRootReducer = require('./reducers');
+//     store.replaceReducer(
+//       persistReducer(persistConfig, connectRouter(history)(nextRootReducer))
+//     );
+//   });
+// }
+
+export const persistor = persistStore(store);
