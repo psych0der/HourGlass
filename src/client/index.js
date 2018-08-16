68307b26bcccc6df30e2a29b764faa8251468ae1
psych0der <mayankbhola@gmail.com>
2018-08-17 00:23:50 +0530

    remove resin

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { Provider as ResinProvider } from 'rendition';
 import { ConnectedRouter } from 'connected-react-router';
 import store, { history } from './redux/store';
 import { App } from './containers';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 import './index.css';
 
@@ -14,11 +15,9 @@ const target = document.querySelector('#root');
 render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <ResinProvider>
-        <div>
-          <App />
-        </div>
-      </ResinProvider>
+      <div>
+        <App />
+      </div>
     </ConnectedRouter>
   </Provider>,
   target
