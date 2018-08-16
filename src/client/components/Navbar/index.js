7ad1fefa9b94bc44bc04fc54de3be0d725e48400
psych0der <mayankbhola@gmail.com>
2018-08-17 00:23:41 +0530

    add navbar

---
+++ src/client/components/Navbar/index.js
@@ -0,0 +1,34 @@
+// @flow
+import React from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import styles from './index.css';
+type Props = {};
+
+const NavBar = (props: Props) => {
+  return (
+    <Navbar collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#brand">HourGlass</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <NavItem eventKey={1} href="#">
+            Home
+          </NavItem>
+        </Nav>
+        <Nav pullRight>
+          <NavItem eventKey={1} href="#">
+            Login
+          </NavItem>
+          <NavItem eventKey={2} href="#">
+            Register
+          </NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+export default NavBar;
