ec831fd2d8a5ee1de21d02efd9c2349a85349d6f
psych0der <mayankbhola@gmail.com>
2018-08-17 03:17:16 +0530

    expose auth

--- src/client/redux/reducers/index.js
+++ src/client/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 
 import register from './register';
+import auth from './auth';
 
-export default combineReducers({ register });
+export default combineReducers({ register, auth });
