d5ecf0bfd107563b788f582a7afead89de16f737
psych0der <mayankbhola@gmail.com>
2018-08-17 00:25:25 +0530

    update indexes

---
+++ src/client/components/index.js
@@ -0,0 +1,3 @@
+export { default as Navbar } from './Navbar';
+export { default as NotFound } from './NotFound';
+export { default as LoaderButton } from './LoaderButton';
--- src/client/containers/index.js
+++ src/client/containers/index.js
@@ -1,2 +1,4 @@
 export { default as App } from './App';
 export { default as Home } from './Home';
+export { default as Login } from './Login';
+export { default as Signup } from './Signup';
