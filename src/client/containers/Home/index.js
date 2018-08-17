a46c0bb6002b7c1a4628ae3e01b7c460c637d20c
psych0der <mayankbhola@gmail.com>
2018-08-17 14:59:09 +0530

    add buttons for time tracks and users

--- src/client/containers/Home/index.js
+++ src/client/containers/Home/index.js
@@ -1,9 +1,27 @@
 // @flow
 import React from 'react';
-import { Navbar } from '../../components';
-import styles from './index.css';
+import {
+  userIsSuperAdmin,
+  userIsAuthenticated,
+  userIsSuperAdminOrUserManager,
+} from '../../commons/authWrapper';
+import { Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import './index.css';
 type Props = {};
 type State = {};
+
+const ManageUsersButton = userIsSuperAdminOrUserManager(() => (
+  <LinkContainer to="/users">
+    <Button bsStyle="warning">Manage users</Button>
+  </LinkContainer>
+));
+
+const ManageTimeTracksButton = userIsAuthenticated(() => (
+  <LinkContainer to="/time-tracks">
+    <Button bsStyle="primary">Manage Time tracks</Button>
+  </LinkContainer>
+));
 
 export class Home extends React.Component<Props, State> {
   state = {};
@@ -14,6 +32,14 @@ export class Home extends React.Component<Props, State> {
         <div className="lander">
           <h1>HourGlass</h1>
           <p>A simple time management app</p>
+          <div className="HomeActions">
+            <div>
+              <ManageUsersButton />
+            </div>
+            <div className="mtop2">
+              <ManageTimeTracksButton />
+            </div>
+          </div>
         </div>
       </div>
     );
