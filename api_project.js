899725a8cdee4298d03c61e28d8c3958c455818a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-30 20:48:24 +0000

    Deploy psych0der/HourGlass to github.com/psych0der/HourGlass.git:gh-pages

---
+++ api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "HourGlass",
+  "version": "0.1.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-11-30T20:45:21.547Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
